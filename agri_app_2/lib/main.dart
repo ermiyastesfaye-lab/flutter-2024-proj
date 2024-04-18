888818489078a2a9b42e19cb39f7022f72365ded
Naol <149867090+Naol7@users.noreply.github.com>
2024-04-18 19:39:41 +0300

    Update main.dart

--- agri_app_2/lib/main.dart
+++ agri_app_2/lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:agri_app_2/screens/presentation/ItemPage.dart';
-import 'package:agri_app_2/screens/presentation/screen_one.dart';
 import 'package:flutter/material.dart';
 import './screens/presentation/dash_board.dart';
 
 void main() {
-  runApp(MaterialApp(home: ScreenOne()));
+  runApp(MaterialApp(home: DashBoardScreen()));
 }
